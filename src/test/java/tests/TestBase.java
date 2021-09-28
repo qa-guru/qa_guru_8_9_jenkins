66daeed265c47ed73fc8675c43d907377810e853
svasenkov <aanher@gmail.com>
2021-09-28 21:25:34 +0300

    added attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,10 +1,13 @@
-package tests.demoqa;
+package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.Attach;
+
 
 public class TestBase {
     @BeforeAll
@@ -18,4 +21,13 @@ public class TestBase {
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
 }
--- src/test/java/tests/demoqa/RegistrationFormTests.java
+++ src/test/java/tests/demoqa/RegistrationFormTests.java
@@ -2,6 +2,7 @@ package tests.demoqa;
 
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import tests.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
