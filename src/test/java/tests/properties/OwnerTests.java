399dd866cdd2b49e2adb785a4cd8768f800bf988
svasenkov <aanher@gmail.com>
2021-10-01 21:19:36 +0300

    added OwnerTests

---
+++ src/test/java/tests/properties/OwnerTests.java
@@ -0,0 +1,24 @@
+package tests.properties;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+public class OwnerTests {
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void readCredentialsTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = format("i login as %s with password %s", login, password);
+        System.out.println(message);
+    }
+}
